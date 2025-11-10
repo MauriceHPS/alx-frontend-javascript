8c8a9a0c5af83f5fe264e9902c2747a4075cb055
MauriceHPS <maurice@headpoint.info>
2025-11-10 17:29:13 +0300

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -73,7 +73,8 @@ type Subjects = "Math" | "History";
 function teachClass(todayClass: Subjects) {
   if (todayClass === "Math") {
     return "Teaching Math";
-  } else {
+  }
+  if (todayClass === "History") {
     return "Teaching History";
   }
 }
