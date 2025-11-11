7b19d6e2441529cffe91d6c6c4bf44918edc75d3
MauriceHPS <maurice@headpoint.info>
2025-11-11 11:27:43 +0300

    Implemented the namespaces challenge. [task_3]

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,1 @@
+import { RowElement, RowID } from "./interface";
---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,7 @@
+export type RowID = 0;
+
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
