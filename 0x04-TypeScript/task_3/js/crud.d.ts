46fd81cc71a89a83f441e173c43d91e6269a5d81
MauriceHPS <maurice@headpoint.info>
2025-11-11 11:40:50 +0300

    Implemented the namespaces challenge. [task_3] debugging

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,1 +1,7 @@
 import { RowElement, RowID } from "./interface";
+
+function insertRow(row: RowElement): RowID {}
+
+function updateRow(rowId: RowID, row: RowElement): void {}
+
+function deleteRow(rowId: RowID): number {}
